6d7700dc5dee4031b2209881f37e51dc0ade5416
axelKeizoStahl <axelkeizo@gmail.com>
2023-08-09 14:44:16 -0400

    support for server abrupt closing

--- server/server.go
+++ server/server.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+    "io"
 	"bufio"
 	"fmt"
 	"net"
+    "os"
+    "os/signal"
 )
 
 func Server() {
@@ -14,20 +17,41 @@ func Server() {
     }
     defer func() { _ = listener.Close() }()
 
+    exit := make(chan os.Signal, 1)
+    signal.Notify(exit, os.Interrupt)
+    go func() {
+        for range exit {
+            fmt.Println()
+            listener.Close()
+            os.Exit(1)
+        }
+    }()
+
     for {
         conn, err := listener.Accept()
         if err!=nil {
             fmt.Println(err)
         }
         go func(c net.Conn) {
+            exit := make(chan os.Signal, 1)
+            signal.Notify(exit, os.Interrupt)
+            go func() {
+                for range exit {
+                    fmt.Println()
+                    c.Close()
+                    os.Exit(1)
+                }
+            }()
             defer c.Close()
             for {
-                message, _ := bufio.NewReader(conn).ReadString('\n')
-                if message != "" {
-                    fmt.Println("Message Received: ", string(message))
-                } else {
-                    break
+                message, err := bufio.NewReader(conn).ReadString('\n')
+                if err != nil {
+                    if err != io.EOF {
+                        fmt.Println(err)
+                    }
+                    return
                 }
+                fmt.Println("Message Received: ", string(message))
             }
         }(conn)
     }
