253c75d6733345fc9d0203a7103b01b8a1a2d3a1
axelKeizoStahl <axelkeizo@gmail.com>
2023-08-09 15:57:43 -0400

    added creation and adding to rooms

--- server/server.go
+++ server/server.go
@@ -1,13 +1,19 @@
 package server
 
 import (
-    "io"
 	"bufio"
 	"fmt"
+	"io"
 	"net"
-    "os"
-    "os/signal"
+	"os"
+	"os/signal"
+	"regexp"
+	"sync"
 )
+
+type Room struct {
+    Connections []net.Conn
+}
 
 func Server() {
     fmt.Println("Starting server...")
@@ -16,6 +22,8 @@ func Server() {
         fmt.Println(err)
     }
     defer func() { _ = listener.Close() }()
+    user_rooms := make(map[string]*Room)
+    var room_mutex sync.Mutex
 
     exit := make(chan os.Signal, 1)
     signal.Notify(exit, os.Interrupt)
@@ -44,12 +52,26 @@ func Server() {
             }()
             defer c.Close()
             for {
-                message, err := bufio.NewReader(conn).ReadString('\n')
+                message, err := bufio.NewReader(c).ReadString('\n')
                 if err != nil {
                     if err != io.EOF {
                         fmt.Println(err)
                     }
                     return
+                }
+
+                room_assignment, _ := regexp.Compile("room_assign: .*")
+                if room_assignment.MatchString(message) {
+                    go func() {
+                        room_mutex.Lock()
+                        defer room_mutex.Unlock()
+                        if _, exists := user_rooms[message[14:]]; !exists {
+                            user_rooms[message[14:]] = &Room{}
+                        }
+                        room := user_rooms[message[14:]]
+                        room.Connections = append(user_rooms[message[14:]].Connections, c)
+                    }()
+                    continue
                 }
                 fmt.Println("Message Received: ", string(message))
             }
