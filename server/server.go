78e868cb633998d33d583f437042963ffc9ece2d
axelKeizoStahl <axelkeizo@gmail.com>
2023-08-08 22:49:43 -0400

    allowed multiple connections to server and fixed buffer panic

--- server/server.go
+++ server/server.go
@@ -8,12 +8,27 @@ import (
 
 func Server() {
     fmt.Println("Starting server...")
-    ln, _ := net.Listen("tcp", ":8000")
-    conn, _ := ln.Accept()
-    defer conn.Close()
+    listener, err := net.Listen("tcp", "127.0.0.1:8000")
+    if err!=nil {
+        fmt.Println(err)
+    }
+    defer func() { _ = listener.Close() }()
 
     for {
-        message, _ := bufio.NewReader(conn).ReadString('\n')
-        fmt.Println("Message Received: ", string(message))
+        conn, err := listener.Accept()
+        if err!=nil {
+            fmt.Println(err)
+        }
+        go func(c net.Conn) {
+            defer c.Close()
+            for {
+                message, _ := bufio.NewReader(conn).ReadString('\n')
+                if message != "" {
+                    fmt.Println("Message Received: ", string(message))
+                } else {
+                    break
+                }
+            }
+        }(conn)
     }
 }
