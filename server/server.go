881db178abb94a59a6d31eee3a6847676e8764c0
axelKeizoStahl <axelkeizo@gmail.com>
2023-08-22 16:47:24 -0400

    continued editing codebase

--- server/server.go
+++ server/server.go
@@ -28,7 +28,8 @@ type User struct {
 type Room struct {
     public bool
     Users []User
-    host User
+    Host User
+    Room_mutex sync.Mutex
 }
 
 func (r *Room) Broadcast(message string) {
@@ -58,48 +59,52 @@ func ListRooms(rooms map[string]*Room) {
 type Listener struct {
     Protocol string
     Port string
-    user_rooms map[string]*Room
+    User_rooms map[string]*Room
+    Ln net.Listener
+    Err error
+}
+
+func (l *Listener) Listen() {
+    l.Ln, l.Err = net.Listen(l.Protocol, l.Port)
+    if l.Err != nil {
+        fmt.Println(l.Err)
+    }
 }
 
 func (l *Listener) Exit() {
     fmt.Println("Closing all connections")
-    for _, room := range l.user_rooms {
-        go func() {
-            go func() { room.Broadcast("Server is shutting down...\n") }()
+    for _, room := range l.User_rooms {
+        go func(room *Room) {
+            go func(room *Room) { room.Broadcast("Server is shutting down...\n") }(room)
             for _, user := range room.Users {
-                go func() {
+                go func(user User) {
                     user.Connection.Close()
                     fmt.Println("Closed connection to", user.Name)
-                }()
+                }(user)
             }
-        }()
+        }(room)
     }
-    fmt.Println("Exiting...")
+    fmt.Println("Closing server listener...")
+    l.Ln.Close()
+    fmt.Println("Exiting")
     os.Exit(1)
 }
 
-
 func Server() {
     fmt.Println("Starting server...")
-    listener, err := net.Listen("tcp", "127.0.0.1:8000")
-    if err!=nil {
-        fmt.Println(err)
-    }
-    defer func() { _ = listener.Close() }()
-    var room_mutex sync.Mutex
+    listener := Listener{Protocol: "tcp", Port: ":8000", User_rooms: make(map[string]*Room)}
+    listener.Listen()
 
     exit := make(chan os.Signal, 1)
     signal.Notify(exit, os.Interrupt)
     go func() {
         for range exit {
-            fmt.Println()
-            listener.Close()
-            os.Exit(1)
+            listener.Exit()
         }
     }()
 
     for {
-        conn, err := listener.Accept()
+        conn, err := listener.Ln.Accept()
         if err!=nil {
             fmt.Println(err)
         }
@@ -125,8 +130,8 @@ func Server() {
 
                 is_exit, _ := regexp.Compile("83479256exit")
                 if is_exit.MatchString(message) {
-                    room_mutex.Lock()
-                    room := user_rooms[message[12:len(message)-1]]
+                    room := listener.User_rooms[message[12:len(message)-1]]
+                    room.Room_mutex.Lock()
                     for index, element := range room.Users {
                         if element.Connection == c {
                             room.Users[index] = room.Users[len(room.Users)-1]
@@ -141,15 +146,16 @@ func Server() {
                 room_assignment, _ := regexp.Compile("room_assign: ")
                 if room_assignment.MatchString(message) {
                     go func() {
-                        room_mutex.Lock()
-                        defer room_mutex.Unlock()
                         room_index := room_assignment.FindStringIndex(message)
                         room_name := message[room_index[1]:len(message)-1]
+                        fmt.Println(room_name)
                         user_name := message[:room_index[0]]
-                        if _, exists := user_rooms[room_name]; !exists {
-                            user_rooms[room_name] = &Room{}
+                        if _, exists := listener.User_rooms[room_name]; !exists {
+                            listener.User_rooms[room_name] = &Room{}
                         }
-                        room := user_rooms[room_name]
+                        room := listener.User_rooms[room_name]
+                        room.Room_mutex.Lock()
+                        defer room.Room_mutex.Unlock()
                         user := User{Name: user_name, Connection: c}
                         room.Users = append(room.Users, user)
                     }()
@@ -158,11 +164,12 @@ func Server() {
                 breaker, _ := regexp.Compile("83479256")
                 breakpoints := breaker.FindAllStringSubmatchIndex(message, -1)
                 user_name := message[:breakpoints[0][0]]
-                room := user_rooms[message[breakpoints[0][1]:breakpoints[1][0]]]
+                room := listener.User_rooms[message[breakpoints[0][1]:breakpoints[1][0]]]
+                fmt.Println(room, message[breakpoints[0][1]:breakpoints[1][0]])
                 message = "[" + user_name + "] " + message[breakpoints[1][1]:]
-                room_mutex.Lock()
+                room.Room_mutex.Lock()
+                defer room.Room_mutex.Unlock()
                 room.Broadcast(message)
-                room_mutex.Unlock()
             }
         }(conn)
     }
