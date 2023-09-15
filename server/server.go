281eb90791984bb480427e7834d363e5410ab3f4
axelKeizoStahl <axelkeizo@gmail.com>
2023-09-15 08:30:04 -0400

    starting class listening

--- server/server.go
+++ server/server.go
@@ -91,6 +91,63 @@ type Room struct {
     Room_mutex sync.Mutex
 }
 
+func (room *Room) HandleMessage(c net.Conn, listener *Listener) {
+
+    defer c.Close()
+    for {
+        message, err := bufio.NewReader(c).ReadString('\n')
+        if err != nil {
+            if err != io.EOF {
+                fmt.Println(err)
+            }
+            return
+        }
+
+        is_exit, _ := regexp.Compile("83479256exit")
+        if is_exit.MatchString(message) {
+            room := listener.User_rooms[message[12:len(message)-1]]
+            room.Room_mutex.Lock()
+            for index, element := range room.Users {
+                if element.Connection == c {
+                    room.Users[index] = room.Users[len(room.Users)-1]
+                    room.Users = room.Users[:len(room.Users)-1]
+                    room.Users = append(room.Users[:index], room.Users[index+1:]...)
+                }
+            }
+            c.Close()
+        }
+
+
+        breaker, _ := regexp.Compile("83479256")
+        breakpoints := breaker.FindAllStringSubmatchIndex(message, -1)
+        user_name := message[:breakpoints[0][0]]
+        room := listener.User_rooms[message[breakpoints[0][1]:breakpoints[1][0]]]
+        fmt.Println(room, message[breakpoints[0][1]:breakpoints[1][0]])
+        message = "[" + user_name + "] " + message[breakpoints[1][1]:]
+        room.Room_mutex.Lock()
+        defer room.Room_mutex.Unlock()
+        room.Broadcast(message)
+
+        room_assignment, _ := regexp.Compile("room_assign: ")
+        if room_assignment.MatchString(message) {
+            go func() {
+                room_index := room_assignment.FindStringIndex(message)
+                room_name := message[room_index[1]:len(message)-1]
+                fmt.Println(room_name)
+                user_name := message[:room_index[0]]
+                if _, exists := listener.User_rooms[room_name]; !exists {
+                    listener.User_rooms[room_name] = &Room{}
+                }
+                room := listener.User_rooms[room_name]
+                room.Room_mutex.Lock()
+                defer room.Room_mutex.Unlock()
+                user := User{Name: user_name, Connection: c}
+                room.Users = append(room.Users, user)
+            }()
+            continue
+        }
+    }
+}
 func (r *Room) Broadcast(message string) {
     for _, conn := range r.Users {
         _, err := conn.Connection.Write([]byte(message))
@@ -137,8 +194,7 @@ func (l *Listener) Handle() {
         if err != nil {
             fmt.Println(err)
         }
-        go
-        go func(c net.Conn) {
+        go func(c net.Conn, listener *Listener) {
             defer c.Close()
             for {
                 message, err := bufio.NewReader(c).ReadString('\n')
@@ -148,9 +204,24 @@ func (l *Listener) Handle() {
                     }
                     return
                 }
-                fmt.Println(message)
+                room_assignment, _ := regexp.Compile("room_assign: ")
+                if room_assignment.MatchString(message) {
+                    room_index := room_assignment.FindStringIndex(message)
+                    room_name := message[room_index[1]:len(message)-1]
+                    fmt.Println(room_name)
+                    user_name := message[:room_index[0]]
+                    if _, exists := l.User_rooms[room_name]; !exists {
+                        l.User_rooms[room_name] = &Room{}
+                    }
+                    room := l.User_rooms[room_name]
+                    room.Room_mutex.Lock()
+                    defer room.Room_mutex.Unlock()
+                    user := User{Name: user_name, Connection: c}
+                    room.Users = append(room.Users, user)
+                }
             }
-        }(conn)
+
+        }(conn, l)
     }
 }
 
